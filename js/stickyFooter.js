bd43f9a97de9a7fa0f2292f64928bfa31e6e6289
Edbert Voo <developer@edbertvoo.com>
2016-08-31 15:35:11 -0600

    Initial Commit
    Initial commit of website for migration

---
+++ js/stickyFooter.js
@@ -0,0 +1,15 @@
+/**
+ * Created by edbertvoo on 2015-12-29.
+ */
+$(window).bind("load", function () {
+    var footer = $("#footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
